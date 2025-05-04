e0c4ab4f1078f9919058dc1675a8e9d872d21c82
kralion <joan300501@gmail.com>
2025-05-04 13:19:01 -0500

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+
+
+    <div className="flex flex-col items-center justify-center h-screen">
+
+      <p className="text-3xl font-bold">
+        Electronics
+      </p>
+    </div>
+
+  )
+}
+
+export default App
