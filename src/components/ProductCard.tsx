9d7bf3a15d374b1935fc60b5132e229fa15fd92a
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2025-05-06 19:37:12 -0500

    ajustar clase de ancho en el componente ProductCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,7 +7,7 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
 
   return (
     <Link to={`/producto/${product?.id}`}>
-      <div className="cursor-pointer border-[0.5px] border-gray-300 rounded-xl overflow-hidden p-4 text-center md:w-[230px]  bg-white hover:shadow-lg transition-all active:opacity-70 hover:opacity-70">
+      <div className="cursor-pointer border-[0.5px] border-gray-300 rounded-xl overflow-hidden p-4 text-center w-[230px]  bg-white hover:shadow-lg transition-all active:opacity-70 hover:opacity-70">
         <img src={product?.acf?.imagen_del_producto.url} alt={product?.title.rendered} className="w-full h-48 object-contain mb-4" />
         <p className="text-red-600 font-[1000] text-lg">S/. {(typeof product?.acf?.precio_descuento === 'number' ? product.acf.precio_descuento : Number(product?.acf?.precio_descuento)).toFixed(2)}</p>
         <p className=" text-[13px] font-medium text-gray-800 mb-2">{product?.title.rendered}</p>
